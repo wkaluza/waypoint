7a37ae4e6e257f898e06b84632fcef0250efb9e5
Wojciech Kaluza <wkaluza@protonmail.com>
2025-04-24 14:02:46 +0200

    Deduplicate autorun macro

--- src/waypoint/include/waypoint/waypoint.hpp
+++ src/waypoint/include/waypoint/waypoint.hpp
@@ -20,15 +20,19 @@ using AutorunFn = void (*)(TestEngine &);
 
 #define X_INTERNAL_MERGE(name, counter, line) name##_##counter##_##line
 
-#define X_INTERNAL_WAYPOINTTESTS_impl(engine, counter, line) \
-  void X_INTERNAL_MERGE(WAYPOINTTESTS, counter, line)(waypoint::TestEngine &); \
+#define X_INTERNAL_WAYPOINT_TEST_NAME(counter, line) \
+  X_INTERNAL_MERGE(WAYPOINT_TEST, counter, line)
+#define X_INTERNAL_WAYPOINT_TEST_PTR_NAME(counter, line) \
+  X_INTERNAL_MERGE(WAYPOINT_TEST_PTR, counter, line)
+
+#define X_INTERNAL_WAYPOINT_AUTORUN_impl(engine, counter, line) \
+  void X_INTERNAL_WAYPOINT_TEST_NAME(counter, line)(waypoint::TestEngine &); \
   waypoint::internal::AutorunFn \
-    __attribute__((used, section("waypoint_tests"))) X_INTERNAL_MERGE( \
-      WAYPOINTTESTSPTR, \
-      counter, \
-      line) = X_INTERNAL_MERGE(WAYPOINTTESTS, counter, line); \
-  void X_INTERNAL_MERGE(WAYPOINTTESTS, counter, line)( \
+    __attribute__((used, section("waypoint_tests"))) \
+    X_INTERNAL_WAYPOINT_TEST_PTR_NAME(counter, line) = \
+      X_INTERNAL_WAYPOINT_TEST_NAME(counter, line); \
+  void X_INTERNAL_WAYPOINT_TEST_NAME(counter, line)( \
     waypoint::TestEngine & (engine))
 
 #define WAYPOINT_TESTS(engine) \
-  X_INTERNAL_WAYPOINTTESTS_impl(engine, __COUNTER__, __LINE__)
+  X_INTERNAL_WAYPOINT_AUTORUN_impl(engine, __COUNTER__, __LINE__)
