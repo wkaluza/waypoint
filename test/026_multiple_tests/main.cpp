9ef3e5af0a8bb4f346e0842b438a907e95905f48
Wojciech Kaluza <wkaluza@protonmail.com>
2025-06-15 04:29:36 +0200

    Add test

---
+++ test/026_multiple_tests/main.cpp
@@ -0,0 +1,51 @@
+#include "waypoint/waypoint.hpp"
+
+namespace
+{
+
+WAYPOINT_AUTORUN(t)
+{
+  auto g1 = t.group("Test group 1");
+
+  t.test(g1, "Test 1")
+    .run(
+      [](auto &ctx)
+      {
+        ctx.assert(true);
+      });
+
+  t.test(g1, "Test 2")
+    .run(
+      [](auto &ctx)
+      {
+        ctx.assert(true);
+      });
+
+  t.test(g1, "Test 3")
+    .run(
+      [](auto &ctx)
+      {
+        ctx.assert(true);
+      });
+}
+
+} // namespace
+
+auto main() -> int
+{
+  auto t = waypoint::make_default_engine();
+
+  bool const success = initialize(t);
+  if(!success)
+  {
+    return 1;
+  }
+
+  auto const results = run_all_tests(t);
+  if(!results.pass())
+  {
+    return 1;
+  }
+
+  return 0;
+}
