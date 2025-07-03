b3fe6dfd91254faea1efe5c0b1588b8061e5e8c9
Wojciech Kaluza <wkaluza@protonmail.com>
2025-07-03 17:50:39 +0200

    Add more assertions to test

--- test/030_capturing_lambda_as_test_body/main.cpp
+++ test/030_capturing_lambda_as_test_body/main.cpp
@@ -1,30 +1,65 @@
 #include "waypoint/waypoint.hpp"
 
 #include <functional>
+#include <memory>
 #include <string>
+#include <utility>
 #include <vector>
 
 namespace
 {
 
+class CopyableImmovable
+{
+public:
+  ~CopyableImmovable() = default;
+  CopyableImmovable() = default;
+  CopyableImmovable(CopyableImmovable const &other) = default;
+  CopyableImmovable(CopyableImmovable &&other) noexcept = delete;
+  auto operator=(CopyableImmovable const &other)
+    -> CopyableImmovable & = default;
+  auto operator=(CopyableImmovable &&other) noexcept
+    -> CopyableImmovable & = delete;
+
+  int foo{42};
+};
+
 WAYPOINT_AUTORUN(t)
 {
+  int const *const captured0 = nullptr;
   std::string const captured1 = "42";
   std::vector<int> const captured2 = {1, 2, 3, 4};
   std::function<int()> const captured3 = []()
   {
     return 123;
   };
+  auto captured4 = std::make_unique<int>(42);
+  auto captured5 = CopyableImmovable{};
 
-  auto g1 = t.group("Test group 1");
+  auto g1 = t.group("Capturing lambdas as test bodies");
 
-  t.test(g1, "Test 1")
+  t.test(g1, "Basic capture")
     .run(
-      [=](waypoint::Context &ctx)
+      [captured0, captured1, captured2, captured3](waypoint::Context &ctx)
       {
+        ctx.assert(captured0 == nullptr);
         ctx.assert(captured1 == "42");
         ctx.assert(captured2.size() == 4);
         ctx.assert(captured3() == 123);
+      });
+
+  t.test(g1, "Movable & non-copyable capture")
+    .run(
+      [captured4 = std::move(captured4)](waypoint::Context &ctx)
+      {
+        ctx.assert(*captured4 == 42);
+      });
+
+  t.test(g1, "Copyable & non-movable capture")
+    .run(
+      [captured5](waypoint::Context &ctx)
+      {
+        ctx.assert(captured5.foo == 42);
       });
 }
 
